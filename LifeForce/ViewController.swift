e45f628de9ad43f0abc835c0e786eb7b648962d9
Thomas Down <thomas.a.down@gmail.com>
2017-09-10 11:48:43 +0100

    Initial Commit

---
+++ LifeForce/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LifeForce
+//
+//  Created by Thomas Down on 10/09/2017.
+//  Copyright © 2017 Thomas Down. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
